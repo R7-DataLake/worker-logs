a5b30275a00826d404626c1dee63a147a0ce5196
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-13 09:21:49 +0700

    update: db connection + promise

--- src/models/log.ts
+++ src/models/log.ts
@@ -2,10 +2,17 @@ import getConnection from "../db"
 
 export class LogModel {
 
-  async saveTransactionLog(log: any) {
-    const db = await getConnection()
-    return db('transactions')
-      .insert(log)
+  async saveTransactionLog(log: any): Promise<any> {
+    const db = await getConnection();
+    return new Promise((resolve: any, reject: any) => {
+      db('transactions')
+        .insert(log)
+        .then(() => resolve())
+        .catch((error: any) => reject(error))
+        .finally(async () => {
+          await db.destroy();
+        });
+    });
   }
 
 }
